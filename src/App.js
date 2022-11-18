e73d4efa3eb67637000fe11f8ce1ef8efadbbb97
Nicolas Lagouarde <anlagouarde@gmail.com>
2022-11-18 10:33:52 -0300

    link a item detail y ruta

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<ItemListContainer/>}/> 
         <Route path='/category/:categoryName' element={<ItemListContainer />} /> 
-        <Route path='item/:id' element={<ItemDetailContainer />} />
+        <Route path='/item/:id' element={<ItemDetailContainer />} />
         <Route path='cart' element={<CartWidget />}/> 
           
 
--- src/Pages/ItemDetailContainer/ItemDetailContainer.js
+++ src/Pages/ItemDetailContainer/ItemDetailContainer.js
@@ -1,29 +1,34 @@
 import { useEffect, useState } from 'react';
 import { data } from "../../data/data";
 import ItemDetail from '../../components/ItemDetail/ItemDetail';
+import { useParams } from 'react-router-dom';
 import './ItemDetailContainer.css';
 
 const ItemDetailContainer = () => {
-    const [product, setProduct] = useState();
-  
-    const getProduct = new Promise((resolve, reject) => {
+  const { id } = useParams();
+  const [product, setProduct] = useState();
+
+
+  const getProduct = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve(data)
-    }, 2000);    
-    }); 
-  
-    useEffect(() => {
-        getProduct.then((respuesta)=>{
-            setProduct(respuesta[0]);
-        });
-        // eslint-disable-next-line
-    }, []);
-  
-    return (
-      <div className='message-greeting-container'> 
-        {product && <ItemDetail producto={product}/>}
-      </div>    
-    );
-  }
+      const filteredProduct = data.filter((producto) => {
+        return producto.id == id;
+      });
+      resolve(filteredProduct)
+    }, 1000);
+  });
+
+  useEffect(() => {
+    getProduct.then((respuesta) => {
+      setProduct(respuesta);
+    });
+  }, [id]);
+
+  return (
+    <div className='message-greeting-container'>
+      {product && <ItemDetail producto={product} />}
+    </div>
+  );
+}
 
 export default ItemDetailContainer
