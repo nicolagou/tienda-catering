413c1af100042ed96434a295965a969fb19e6bf4
Nicolas Lagouarde <anlagouarde@gmail.com>
2022-12-14 09:55:46 -0300

    Agregado el precio total de mi carrito

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,13 +1,33 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { cartContext } from '../../context/CartProvider';
 
 
 const Cart = () => {
-    const {cart} = useContext(cartContext);
-    console.log(cart);
-  return (
-    <div>Cart</div>
-  )
+    const { cart } = useContext(cartContext);
+    const [total, setTotal] = useState(0);
+    const getTotalPrice = () => {
+        setTotal(cart.reduce((acc, product) => acc + product.price * product.quantity, 0))
+    };
+
+    useEffect(() => {
+        getTotalPrice();
+               // eslint-disable-next-line
+    }, [cart])
+
+    return (
+        <div className='message-greeting-container'>
+            {cart.map((product) => (
+                <div className="card" kew={product.id}>
+                    <img alt={product.title} src={`/images/${product.imageId}`} />
+                    <h2>Nombre: {product.title}</h2>
+                    <h2>Descripcion: {product.description}</h2>
+                </div>
+            ))}
+            <div>
+                <h1>Total: {total}</h1>
+            </div>
+        </div>
+    )
 }
 
 export default Cart
