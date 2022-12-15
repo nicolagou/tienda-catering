19b8f4e192ac0927895a0b9c0a64710b1ef08e22
Nicolas Lagouarde <anlagouarde@gmail.com>
2022-12-15 12:13:39 -0300

    Agregado el contador visible de productos al carrito

--- src/Pages/CartWidget/CartWidget.js
+++ src/Pages/CartWidget/CartWidget.js
@@ -1,11 +1,14 @@
-import React from 'react';
 import './CartWidget.css';
+import { useContext } from 'react';
+import { cartContext } from '../../context/CartProvider';
 
 const CartWidget = () => {
+    const { cart } = useContext(cartContext)
     return (
         <div className='menu-navbar__logo'>
-            <img className='menu-navbar__img' src={`/images/carritodecompras.png`} alt="cart widget"/>
-    </div>
+            <img className='menu-navbar__img' src={`/images/carritodecompras.png`} alt="cart widget" />
+            <h3>{cart.length>0 && cart.length}</h3>
+        </div>
     )
 }
 
