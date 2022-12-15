c9041fc713ae7ec5ff6258c352e8ee4b7971205e
Nicolas Lagouarde <anlagouarde@gmail.com>
2022-12-15 10:45:12 -0300

    Agregado el Formulario

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,11 +1,17 @@
 import { useContext, useState, useEffect } from "react";
 import { cartContext } from '../../context/CartProvider';
 import { collection, addDoc, getFirestore, doc, updateDoc } from "firebase/firestore";
+import moment from "moment";
 
 
 const Cart = () => {
     const { cart } = useContext(cartContext);
     const [total, setTotal] = useState(0);
+    const [formValues, setFormValues] = useState({
+        name:"",
+        phone:"",
+        email:"",
+    });
 
     const getTotalPrice = () => {
         setTotal(cart.reduce((acc, product) => acc + product.price * product.quantity, 0))
@@ -16,10 +22,11 @@ const Cart = () => {
         const query = collection(db, "order");
         const newOrder = {
             buyer: {
-                name: "nico",
-                phone: "1513135",
-                email: "test@test.com"
+                name: formValues.name,
+                phone: formValues.phone,
+                email: formValues.email,
             },
+            date: moment().format("DD/MM/YYYY"),
             items: cart,
             total: total,
         };
@@ -37,13 +44,19 @@ const Cart = () => {
                 });
             })
             .catch((error) => console.log(error));
-
     };
 
     useEffect(() => {
         getTotalPrice();
         // eslint-disable-next-line
     }, [cart])
+
+    const handleInputChange = (event) =>{
+        setFormValues({
+            ...formValues,
+            [event.target.name]:event.target.value,
+        })
+    };
 
     return (
         <div className='message-greeting-container'>
@@ -57,6 +70,12 @@ const Cart = () => {
             <div>
                 <h1>Total: {total}</h1>
                 <button onClick={createOrder}>Crear Orden</button>
+                <div>
+                    <h2>Formulario</h2>
+                    <input name="name" type="text" placeholder="Nombre" value={formValues.name} onChange={handleInputChange}/>
+                    <input name="phone" type="text" placeholder="Telefono" value={formValues.phone} onChange={handleInputChange}/>
+                    <input name="email" type="text" placeholder="Email" value={formValues.email} onChange={handleInputChange}/>
+                </div>
             </div>
         </div>
     )
