2cfd58ffe47ec37035a61765040ec789aaff558a
Nicolas Lagouarde <anlagouarde@gmail.com>
2022-12-15 09:26:37 -0300

    Agregado insertar orden auto generada

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,17 +1,37 @@
 import { useContext, useState, useEffect } from "react";
 import { cartContext } from '../../context/CartProvider';
+import { collection, addDoc, getFirestore } from "firebase/firestore";
 
 
 const Cart = () => {
     const { cart } = useContext(cartContext);
     const [total, setTotal] = useState(0);
+
     const getTotalPrice = () => {
         setTotal(cart.reduce((acc, product) => acc + product.price * product.quantity, 0))
     };
 
+    const createOrder = () => {
+        const db = getFirestore();
+        const query = collection(db, "order");
+        const newOrder = {
+            buyer: {
+                name: "nico",
+                phone: "1513135",
+                email: "test@test.com"
+            },
+            items: cart,
+            total: total,
+        };
+        addDoc(query, newOrder)
+        .then((response)=>alert(`Orden creada con el id: ${response.id}`))
+        .catch((error)=>console.log(error));
+
+    };
+
     useEffect(() => {
         getTotalPrice();
-               // eslint-disable-next-line
+        // eslint-disable-next-line
     }, [cart])
 
     return (
@@ -25,6 +45,7 @@ const Cart = () => {
             ))}
             <div>
                 <h1>Total: {total}</h1>
+                <button onClick={createOrder}>Crear Orden</button>
             </div>
         </div>
     )
